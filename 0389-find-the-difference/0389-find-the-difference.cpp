f717553abd958e860bdac353d3c36b97d6263846
Krishna Choudhary <Krishnachoudharyjob19@gmail.com>
2026-08-28 23:48:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.42%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char answer = 0;
+
+        for (char ch : s) {
+            answer ^= ch;
+        }
+
+        for (char ch : t) {
+            answer ^= ch;
+        }
+
+        return answer;
+    }
+};
